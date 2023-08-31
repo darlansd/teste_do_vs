7f316fd26a81598d12e0778cc6bcb703389fff57
darlan salino dias <darlansalinodias52@gmail.com>
2023-08-31 13:36:24 -0400

    second

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Fala garaio')
+#Formulário de dados
+#Pega os dados do usuário através dos inputs
+print("Olá, para podermos lhe mostrar seus dados, preciso que preencha alguns dados ok?")
+nome = input("Primeiro preciso que digite seu nome completo: ")
+idade = input("Prazer em te conhecer " + nome +", agora preciso da sua data de nascimento nesse formato (xx/xx/xx): ")
+endereco = input("Muito bem, agora preciso do seu endereço: ")
+
+print("O seu nome é " + nome + ", \n sua data de nascimento é " + idade + ",\n e seu endereço é " + endereco)
+
